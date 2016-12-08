64004ac14717834da5908f8ccd9618c6d2cf23e2
Adrian S MUresan <adriansmuresan@gmail.com>
2016-12-07 20:20:31 -0600

    A user can add, edit and delete a review

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :movies
+  has_many :reviews, dependent: :destroy #if a user deletes the account we want the reviews deleted as well
 end
